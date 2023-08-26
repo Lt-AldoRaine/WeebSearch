1721486b14b1e427fabd7e6eda286bb394576bd2
Aldo Raine <105580292+Lt-AldoRaine@users.noreply.github.com>
2023-08-26 00:49:07 -0500

    feat: Initialize Project

---
+++ houdini.config.js
@@ -0,0 +1,14 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	schemaPath: './schema.graphql',
+	watchSchema: {
+		url: 'https://graphql.anilist.co'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
+
+export default config;
